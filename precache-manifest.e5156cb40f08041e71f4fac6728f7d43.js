53888998340f3140234a759965c7ad0cb13dd685
aroranipun12 <aroranipun1@gmail.com>
2020-05-13 21:37:52 +0530

    Updates

---
+++ precache-manifest.e5156cb40f08041e71f4fac6728f7d43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5336db846ea5e6bfb88716db334479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0005f7dda2348ee7d31",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "04507ca5d8a92242ddba",
+    "url": "/static/css/main.26003a94.chunk.css"
+  },
+  {
+    "revision": "f0005f7dda2348ee7d31",
+    "url": "/static/js/2.8c198139.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.8c198139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04507ca5d8a92242ddba",
+    "url": "/static/js/main.2c8a7d1b.chunk.js"
+  },
+  {
+    "revision": "bfe1b33cac77cf29b598",
+    "url": "/static/js/runtime-main.dbb9999b.js"
+  }
+]);
